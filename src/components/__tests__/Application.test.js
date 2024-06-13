793f43b12c449d27e90819d4d3acb64293f8b6d5
Ben Hood <benhood97@btinternet.com>
2024-06-12 17:50:45 -0700

    implement new tests for errors

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 
 import {
   render,
@@ -11,6 +12,8 @@ import {
   getByAltText,
   getByText,
   queryByText,
+  queryByAltText,
+  findByAltText,
 } from "@testing-library/react";
 
 import Application from "components/Application";
@@ -56,5 +59,115 @@ describe("application", () => {
     );
 
     expect(getByText(day, "no spots remaining"));
+  });
+
+  it("loads data, cancels an interview and increases the spots remaining for Monday by 1", async () => {
+    const { container } = render(<Application />);
+    await findByText(container, "Archie Cohen");
+
+    const appointment = getAllByTestId(container, "appointment").find(
+      (appointment) => queryByText(appointment, "Archie Cohen")
+    );
+
+    fireEvent.click(queryByAltText(appointment, "Delete"));
+
+    expect(
+      getByText(appointment, "Are you sure you would like to delete?")
+    ).toBeInTheDocument();
+
+    fireEvent.click(getByText(appointment, "Confirm"));
+
+    expect(getByText(appointment, "Deleting")).toBeInTheDocument();
+
+    await findByAltText(appointment, "Add");
+
+    const day = getAllByTestId(container, "day").find((day) =>
+      queryByText(day, "Monday")
+    );
+
+    expect(getByText(day, "2 spots remaining"));
+  });
+
+  it("loads data, edits an interview and keeps the spots remaining for Monday the same", async () => {
+    const { container } = render(<Application />);
+    await findByText(container, "Archie Cohen");
+
+    const appointment = getAllByTestId(container, "appointment").find(
+      (appointment) => queryByText(appointment, "Archie Cohen")
+    );
+
+    fireEvent.click(queryByAltText(appointment, "Edit"));
+
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Lydia Miller-Jones" },
+    });
+
+    fireEvent.click(getByAltText(container, "Sylvia Palmer"));
+
+    fireEvent.click(getByText(container, "Save"));
+
+    expect(getByText(appointment, "Saving")).toBeInTheDocument();
+
+    await findByText(appointment, "Lydia Miller-Jones");
+
+    const day = getAllByTestId(container, "day").find((day) =>
+      queryByText(day, "Monday")
+    );
+
+    expect(getByText(day, "1 spot remaining"));
+  });
+
+  it("shows the save error when failing to save an appointment", async () => {
+    axios.put.mockRejectedValueOnce();
+    const { container } = render(<Application />);
+    await findByText(container, "Archie Cohen");
+
+    const appointments = queryAllByTestId(container, "appointment");
+    const appointment = getAllByTestId(container, "appointment")[0];
+
+    fireEvent.click(getByAltText(appointment, "Add"));
+
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Lydia Miller-Jones" },
+    });
+
+    fireEvent.click(getByAltText(container, "Sylvia Palmer"));
+
+    fireEvent.click(getByText(container, "Save"));
+
+    expect(getByText(appointment, "Saving")).toBeInTheDocument();
+
+    await findByText(appointment, "Error");
+
+    expect(
+      getByText(appointment, "Could not book appointment.")
+    ).toBeInTheDocument();
+  });
+
+  it("shows the delete error when failing to delete an existing appointment", async () => {
+    axios.delete.mockRejectedValueOnce();
+
+    const { container } = render(<Application />);
+    await findByText(container, "Archie Cohen");
+
+    const appointment = getAllByTestId(container, "appointment").find(
+      (appointment) => queryByText(appointment, "Archie Cohen")
+    );
+
+    fireEvent.click(queryByAltText(appointment, "Delete"));
+
+    expect(
+      getByText(appointment, "Are you sure you would like to delete?")
+    ).toBeInTheDocument();
+
+    fireEvent.click(getByText(appointment, "Confirm"));
+
+    expect(getByText(appointment, "Deleting")).toBeInTheDocument();
+
+    await findByText(appointment, "Error");
+
+    expect(
+      getByText(appointment, "Could not cancel appointment.")
+    ).toBeInTheDocument();
   });
 });
